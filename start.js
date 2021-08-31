f54de05915a643e6020b48e80c35d122614835fe
natwhiskey <kimngza@hotmail.com>
2021-08-31 23:39:36 +0700

    declare varible and set width,height

--- start.js
+++ start.js
@@ -1,5 +1,12 @@
 (()=> {
 
+    const canvas = document.getElementById('painting');
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    const context = canvas.getContext ('2d'); 
+    
+    let previousPoint = {x: 0, y: 0};
     function run(){
     }
     run();
